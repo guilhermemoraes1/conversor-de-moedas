839761ead4f4403aac6d2936336b7428ffef90cb
guilhermemoraes1 <guilherme.morais.54540218@gmail.com>
2025-05-11 17:54:16 -0300

    add try and catch

--- src/main/java/ConsultaApi.java
+++ src/main/java/ConsultaApi.java
@@ -1,7 +1,6 @@
 import Records.Cotacao;
 import com.google.gson.Gson;
 
-import java.io.IOException;
 import java.net.URI;
 
 import java.net.http.HttpClient;
@@ -10,20 +9,25 @@ import java.net.http.HttpResponse;
 
 public class ConsultaApi {
 
-    public Cotacao mostrarJson() throws IOException, InterruptedException {
+    public Cotacao mostrarJson() {
         // Setting URL
         String url_str = "https://v6.exchangerate-api.com/v6/0b0ec803f24d7d37b1069bd2/latest/USD";
 
-        // Making Request
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url_str))
-                .build();
+        try {
+            // Making Request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url_str))
+                    .build();
 
-        HttpResponse<String> response = HttpClient
-                .newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        // System.out.println(response.body());
-        return new Gson().fromJson(response.body(), Cotacao.class);
+            // System.out.println(response.body());
+            return new Gson().fromJson(response.body(), Cotacao.class);
+        } catch (Exception e ) {
+            throw new RuntimeException("Algum erro ocorreu na API.");
+        }
+
     }
 }
